3aede2e79ce8579f286b35eb6f21dffbdaae73bf
HeitorDenech <heitor.denech@gmail.com>
2026-08-12 22:36:17 -0300

    terceiro commit

--- SmashCo/src/App.tsx
+++ SmashCo/src/App.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+import smash from './assets/smash.avif'
+import smashlogo from './assets/smashlogo.png'
+import smashinferno from './assets/smashinferno.avif'
+import smashsalada from './assets/smashsalada.avif'
+
 function App() {
   const [scrolled, setScrolled] = useState(false)
   const [menuOpen, setMenuOpen] = useState(false)
@@ -28,9 +33,12 @@ function App() {
       <header>
         <section className={`navbar ${scrolled ? 'scrolled' : ''}`}>
 
-          <h1>
-            SMASH <span>&</span> CO.
-          </h1>
+          <div className="navbar-logo">
+            <img src={smashlogo} alt="Logo" />
+            <h1>
+              SMASH <span>&</span> CO.
+            </h1>
+          </div>
 
           {/* BOTÃO HAMBÚRGUER */}
 
@@ -64,7 +72,84 @@ function App() {
       {/* ----------------------------- MAIN ------------------------------ */}
 
       <main>
-        <section></section>
+
+        <section className="hero">
+
+          <div className="hero-content">
+
+            <h1>
+              Aberto agora · Feito na hora
+            </h1>
+
+          </div>
+
+          <h1 id="hero-title">
+            BURGUERS <span> ARTESANAIS</span>.
+          </h1>
+
+          <p id="hero-subtitle">
+            Cada burger feito à mão, prensado na chapa quente e servido em 8 minutos. Sem
+            frescura. Só sabor de verdade.
+          </p>
+
+          <div className="hero-content-btn">
+
+            <button id='ver-cardapio'>Ver cardápio</button>
+
+            <button id='promocoes'>Promoções</button>
+
+          </div>
+
+        </section>
+
+        <section className="menu">
+
+          <div className="menu-content-text">
+
+            <div id="menu-title">
+              <h1>Burguers <span> impressionantes</span></h1>
+            </div>
+
+            <div id="menu-subtitle">
+              <p>Ingredientes selecionados. Carne bovina 100% pura,
+                pão brioche artesanal, molhos autorais.</p>
+            </div>
+
+          </div>
+
+          <div className="menu-content-burguers">
+
+            <div className="menu-content-burguers-card">
+              <div className="menu-content-burguers-card-img">
+                <img src={smash} alt="" />
+              </div>
+              <h1>Smash clássico</h1>
+              <p>Smash duplo de frango, queijo americano, molho especial, alface e tomate.</p>
+              <h2>R$32</h2>
+            </div>
+
+            <div className="menu-content-burguers-card">
+              <div className="menu-content-burguers-card-img">
+                <img src={smashinferno} alt="" />
+              </div>
+              <h1>Smash picante</h1>
+              <p>Smash duplo bovino, queijo americano, bacon, pimenta e picles.</p>
+              <h2>R$32</h2>
+            </div>
+
+            <div className="menu-content-burguers-card">
+              <div className="menu-content-burguers-card-img">
+                <img src={smashsalada} alt="" />
+                <h1>Smash salada</h1>
+                <p>Smash bovino, queijo americano, ketchup, alface, tomate e picles.</p>
+                <h2>R$32</h2>
+              </div>
+            </div>
+
+          </div>
+
+        </section>
+
       </main>
     </>
   )
